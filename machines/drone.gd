630b8a8292b69c36f9b08e43897c56b420c928bd
adrimarco <adrianmarco-@hotmail.es>
2024-11-13 21:11:55 +0100

    feat: add avoidance

--- machines/drone.gd
+++ machines/drone.gd
@@ -2,7 +2,7 @@ class_name Drone extends CharacterBody3D
 
 signal drone_arrived_to_station
 
-var speed:float = 1.0
+var speed:float = 2.0
 var station:DroneStation = null
 var moving_to_target:bool = false
 
@@ -11,6 +11,7 @@ var moving_to_target:bool = false
 
 func _ready() -> void:
 	gl_target.game_location_reached.connect(on_target_enter)
+	nav_agent.max_speed = speed
 
 
 func return_to_station() -> void:
@@ -37,13 +38,16 @@ func _physics_process(_delta: float) -> void:
 func _move_to_target() -> void:
 	var movement_direction := (get_next_pathfinding_location() - global_position).normalized()
 	
-	velocity = movement_direction * speed
-	
-	move_and_slide()
-	
+	nav_agent.velocity = movement_direction * speed
 
 func get_next_pathfinding_location() -> Vector3:
 	if nav_agent.is_navigation_finished():
 		return global_position
 	
 	return nav_agent.get_next_path_position()
+
+
+func _on_velocity_computed(safe_velocity: Vector3) -> void:
+	if moving_to_target:
+		velocity = safe_velocity
+		move_and_slide()
