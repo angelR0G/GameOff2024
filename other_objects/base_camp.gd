f94810d5307f43b12dc2f5e79459514d4846535e
adrimarco <adrianmarco-@hotmail.es>
2024-11-20 21:22:02 +0100

    feat: motorbike can be fully unload from base

--- other_objects/base_camp.gd
+++ other_objects/base_camp.gd
@@ -28,8 +28,17 @@ func add_substract_energy(energy:int) -> void:
 	return
 
 func _interaction() -> void:
+	var motorbike :Motorbike = get_tree().get_nodes_in_group("bike")[0]
+	var can_motorbike_be_unload := func() -> bool:
+		if motorbike.stored_materials.current_weight <= 0:
+			return false
+		
+		var d := global_position.distance_to(motorbike.global_position)
+		return d < 20.0
+	
 	var interactions_ui := InteractionsDisplay.Instance
-	interactions_ui.add_interaction("Store Materials", store_materials.bind(Player.Instance.materials))
+	interactions_ui.add_interaction("Store Materials", store_materials.bind(Player.Instance.materials), Player.Instance.materials.current_weight <= 0)
+	interactions_ui.add_interaction("Unload Motorbike", store_materials.bind(motorbike.stored_materials), not can_motorbike_be_unload.call())
 	interactions_ui.add_close_list_button()
 	interactions_ui.show_list()
 	await interactions_ui.display_closed
