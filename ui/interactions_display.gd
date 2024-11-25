c075c861144446df4f36d0f7824de67ac42d7178
adrimarco <adrianmarco-@hotmail.es>
2024-11-25 21:20:53 +0100

    feat: animate interactions list

--- ui/interactions_display.gd
+++ ui/interactions_display.gd
@@ -1,6 +1,6 @@
 class_name InteractionsDisplay extends CanvasLayer
 
-const interaction_button := preload("res://ui/interaction_button.tscn")
+const INTERACTION_BUTTON := preload("res://ui/interaction_button.tscn")
 @onready var interactions_list := $InteractionsList
 
 static var Instance :InteractionsDisplay = null
@@ -11,17 +11,30 @@ func _ready() -> void:
 	Instance = self
 
 func add_interaction(text:String, function:Callable, disabled:bool = false) -> void:
-	var new_interaction :Button = interaction_button.instantiate()
-	interactions_list.add_child(new_interaction)
+	var new_interaction :Button = INTERACTION_BUTTON.instantiate()
+	var new_control_node :Control = Control.new()
 	
+	# Configure control node (button's parent)
+	new_control_node.custom_minimum_size = new_interaction.custom_minimum_size
+	
+	# Add to hierarchy
+	interactions_list.add_child(new_control_node)
+	new_control_node.add_child(new_interaction)
+	
+	# Configure interaction button
 	new_interaction.text = text
 	new_interaction.disabled = disabled
 	new_interaction.pressed.connect(func () -> void:
-		clear_list()
+		await clear_list()
 		if function.is_valid():
 			await function.call()
 		display_closed.emit()
 	)
+	
+	# Animate button
+	new_interaction.scale = Vector2.ZERO
+	var tween := get_tree().create_tween()
+	tween.tween_property(new_interaction, "scale", Vector2.ONE, 0.2).set_delay((interactions_list.get_child_count()-1)*0.05).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 
 func add_close_list_button() -> void:
 	add_interaction("Close", Callable())
@@ -35,12 +48,16 @@ func show_list() -> void:
 	interactions_list.mouse_filter = Control.MOUSE_FILTER_PASS
 
 func clear_list() -> void:
-	hide_list()
+	var tween := get_tree().create_tween().set_parallel(true)
 	for child in interactions_list.get_children():
-		child.queue_free()
+		var child_button := child.get_child(0)
+		tween.tween_property(child_button, "scale", Vector2.ZERO, 0.2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+		tween.tween_callback(child.queue_free).set_delay(0.2)
 	
+	await tween.finished
+	hide_list()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if interactions_list.visible and event.is_action_pressed("back"):
-		clear_list()
+		await clear_list()
 		display_closed.emit()
